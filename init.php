3d6af6f08da1fa2c497b5cd7e5bea63bc0330d2d
Laurent Cozic <laurent@pogopixels.com>
2013-04-26 19:49:13 +0800

    Renaming for Kohana 3.3

--- init.php
+++ init.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 // Disable in CLI
-if (Kohana::$is_cli)
+if (defined("PHP_SAPI") && PHP_SAPI == 'cli')
 	return;
 
 Route::set('logdelete', 'logs/delete/<year>/<month>/<logfile>', array('logfile' => '.*'))
